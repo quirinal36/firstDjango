6744f2ebd519ea8884e8113c0701466b8494c3e5
turboguy <turboguy36@gmail.com>
2019-08-24 23:12:59 +0900

    Initial commit

---
+++ app_default/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Question
+from .models import Choice
+
+
+admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets =[
+        (None,  {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    list_display = ('question_text', 'pub_date','was_published_recently')
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
